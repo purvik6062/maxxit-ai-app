fb4d27621fbd893408a817c68d71eca6643c901e
purvik6062 <purvikpanchal6336@gmail.com>
2025-01-21 15:22:18 +0530

    added types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,37 +35,6 @@ interface NetworkData {
   [key: string]: any;
 }
 
-// Props interfaces for components
-interface NetworksProps {
-  networkName: string;
-  setNetworkName: (name: string) => void;
-  notifyError: (msg: string) => void;
-  notifySuccess: (msg: string) => void;
-}
-
-interface TradingProps {
-  axios: any;
-  trading: any[];
-  tradingCount: number;
-  length: number;
-  setTradingCount: (count: number) => void;
-  setActiveComponent: (component: string) => void;
-  notifyError: (msg: string) => void;
-  notifySuccess: (msg: string) => void;
-}
-
-interface PriceProps {
-  buyMemberShip: (memberType: string, price: number) => void;
-  setMembershipType: (type: string) => void;
-}
-
-interface AuthProps {
-  setActiveComponent: (component: string) => void;
-  axios: any;
-  notifyError: (msg: string) => void;
-  notifySuccess: (msg: string) => void;
-}
-
 const HomePage: React.FC = () => {
   // STATE VARIABLES
   const [activeComponent, setActiveComponent] = useState<string>("Home");
