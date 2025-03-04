6576b735695d13bf8bd971a2c706a3f734ce2808
purvik6062 <purvikpanchal6336@gmail.com>
2025-03-04 12:32:03 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,21 @@ import {
   ImpactLeaderboard,
   HeartbeatDashboard,
   Home,
-  Footer
+  Footer,
 } from "../components/index";
 
 const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-[#020617]">
       <Header />
-      
+
       <main className="flex-grow px-6 py-8 max-w-screen-2xl mx-auto w-full">
         <div className="flex flex-col gap-8">
-          
-          {/* Flex layout for dashboard components with customizable widths */}
           <div className="flex flex-col lg:flex-row gap-6">
-            {/* Left component - ImpactLeaderboard with customizable width */}
             <div className="lg:w-[42%] bg-gray-800/50 rounded-xl backdrop-blur-sm border border-gray-700/50 shadow-lg h-fit">
               <ImpactLeaderboard />
             </div>
-            
-            {/* Right component - HeartbeatDashboard with customizable width */}
+
             <div className="lg:w-[58%] bg-gray-800/50 rounded-xl backdrop-blur-sm border border-gray-700/50 shadow-lg h-fit">
               <HeartbeatDashboard />
             </div>
