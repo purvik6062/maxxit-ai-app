22191751e4883ecb02e7d2ba4820594fbf4ecb5a
purvik6062 <purvikpanchal6336@gmail.com>
2025-05-04 16:26:17 +0530

    Enhancement: Integrate BProgress Library and Update Influencer Metrics
    - Added @bprogress/next dependency to enhance progress tracking features.
    - Refactored Providers component to include ProgressProvider for improved loading indicators.
    - Updated InfluencerMetrics component to handle subscription logic and display subscription confirmation modal.
    - Enhanced SubscriptionsList component for better user experience and visual consistency.
    - Cleaned up code formatting for improved readability across various components.

--- src/app/influencer/[id]/page.tsx
+++ src/app/influencer/[id]/page.tsx
@@ -1,20 +1,27 @@
+"use client";
+import React from "react";
 import SignalStats from "@/components/InfluencerProfile/SignalStats";
 // import InfluencerTable from "@/components/InfluencerProfile/InfluencerTable";
 import InfluencerMetrics from "@/components/InfluencerProfile/InfluencerMetrics";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 interface PageProps {
-  params: Promise<{
+  params: {
     id: string;
-  }>;
+  };
 }
 
-async function page({ params }: PageProps) {
-  const { id } = await params;
+function Page({ params }: PageProps) {
+  const { id } = params;
+
   return (
     <div className="py-4 px-5">
+      <ToastContainer />
       <InfluencerMetrics influencerId={id} />
       <SignalStats influencerId={id} />
     </div>
   );
 }
 
-export default page;
+export default Page;
